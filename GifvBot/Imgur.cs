4522d7c4a3ce360b7aac71a30f84c3a13c5b6f63
Liwen Guo <lg@livven.me>
2015-12-07 23:59:00 +0100

    implement basic functionality

---
+++ GifvBot/Imgur.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using AngleSharp.Dom;
+using AngleSharp.Dom.Html;
+
+namespace GifvBot
+{
+    class Imgur : IDisposable
+    {
+        HttpClient client = new HttpClient();
+
+        public void Dispose()
+        {
+            client.Dispose();
+        }
+
+        public async Task<Uri> GetGifvLinkAsync(Uri url)
+        {
+            try
+            {
+                var response = await GetImgurPageAsync(url);
+                if (response == null)
+                {
+                    return null;
+                }
+                var html = await response.Content.ReadAsHtmlAsync();
+                var twitterStreams = html.QuerySelectorAll(@"meta[name=""twitter:player:stream""]");
+                return GetGifvLink(twitterStreams);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        async Task<HttpResponseMessage> GetImgurPageAsync(Uri url)
+        {
+            url = GetImgurPageLink(url) ?? url;
+            while (true)
+            {
+                var response = await client.GetAsync(url);
+                if (!response.IsSuccessStatusCode)
+                {
+                    return null;
+                }
+                var redirectedUri = response.RequestMessage.RequestUri;
+                if (redirectedUri == url)
+                {
+                    return response;
+                }
+                url = GetImgurPageLink(redirectedUri);
+                if (url == null)
+                {
+                    return response;
+                }
+            }
+        }
+
+        static void Analyze(IHtmlDocument html)
+        {
+            var ogType = html.QuerySelector(@"meta[property=""og:type""]")?.GetAttribute("content");
+            var twitterCard = html.QuerySelector(@"meta[name=""twitter:card""]")?.GetAttribute("content");
+            var ogVideos = GetGifvLink(html.QuerySelectorAll(@"meta[property=""og:video:secure_url""]"));
+            var twitterStreams = GetGifvLink(html.QuerySelectorAll(@"meta[name=""twitter:player:stream""]"));
+        }
+
+        static Uri GetGifvLink(IEnumerable<IElement> list)
+        {
+            foreach (var element in list)
+            {
+                var url = new Uri(element.GetAttribute("content"));
+                if (url.Host == "i.imgur.com")
+                {
+                    var result = new UriBuilder(url);
+                    result.Path = result.Path.Split('.')[0] + ".gifv";
+                    return result.Uri;
+                }
+            }
+            return null;
+        }
+
+        static Uri GetImgurPageLink(Uri url)
+        {
+            if (url.Host != "i.imgur.com")
+            {
+                return null;
+            }
+            var result = new UriBuilder(url);
+            result.Host = "imgur.com";
+            result.Path = result.Path.Split('.')[0];
+            result.Query = "";
+            return result.Uri;
+        }
+    }
+}
--- GifvBot/Program.cs
+++ GifvBot/Program.cs
@@ -1,15 +1,65 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace GifvBot
 {
     class Program
     {
+        static readonly string ClientId;
+        static readonly string Secret;
+        static readonly string RefreshToken;
+
+        static Program()
+        {
+            ClientId = Environment.GetEnvironmentVariable("GIFVBOT_REDDIT_CLIENT_ID");
+            Secret = Environment.GetEnvironmentVariable("GIFVBOT_REDDIT_SECRET");
+            RefreshToken = Environment.GetEnvironmentVariable("GIFVBOT_REDDIT_REFRESH_TOKEN");
+        }
+
         static void Main(string[] args)
         {
+            MainAsync(args).Wait();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    await RunAsync();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                await Task.Delay(TimeSpan.FromSeconds(10));
+            }
+        }
+
+        static async Task RunAsync()
+        {
+            using (var reddit = new Reddit("gifvbot", "lastprocessed"))
+            using (var imgur = new Imgur())
+            {
+                await reddit.AuthenticateAsync(ClientId, Secret, RefreshToken);
+                var items = await reddit.GetNewItemsAsync();
+                var filtered = items.Where(item => (item.Link.Host == "imgur.com" || item.Link.Host.EndsWith(".imgur.com")) && !item.Link.AbsolutePath.EndsWith(".gifv")).ToList();
+                Console.WriteLine($"{items.Count} new items, {filtered.Count} to process, {items.Count - filtered.Count} filtered out");
+                var convertedCount = 0;
+                foreach (var item in filtered)
+                {
+                    var result = await imgur.GetGifvLinkAsync(item.Link);
+                    if (result != null)
+                    {
+                        convertedCount++;
+                        await reddit.PostCommentAsync(item.Name, result);
+                    }
+                }
+                Console.WriteLine($"{convertedCount} links converted");
+            }
         }
     }
 }
